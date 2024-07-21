ad6d279cf707a61a6282ed2be3b29b145967b699
Manoel <fonsecadesouzamanoel@gmail.com>
2024-07-21 11:09:29 -0300

    Refactored working setup

--- src/test/java/com/personal/fragrances/service/FragranceServiceTests.java
+++ src/test/java/com/personal/fragrances/service/FragranceServiceTests.java
@@ -3,20 +3,17 @@ package com.personal.fragrances.service;
 import com.personal.fragrances.domain.Fragrance;
 import com.personal.fragrances.repository.FragranceRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @SpringBootTest
+@Import(TestcontainersConfiguration.class)
 @ActiveProfiles("test")
 public class FragranceServiceTests {
 
@@ -25,27 +22,6 @@ public class FragranceServiceTests {
 
     @Autowired
     private FragranceService fragranceService;
-
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-            "postgres:16-alpine"
-    );
-
-    @BeforeAll
-    static void beforeAll() {
-        postgres.start();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        postgres.stop();
-    }
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
 
     @Test
     void shouldRetrieveFragrance() {
